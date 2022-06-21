b80e59f2ab970377b45fa6f70b0c7b25c485ab45
Floriano200404 <florianoneto2004@gmail.com>
2022-06-21 18:42:16 -0400

    Atualizando texto e estilizando tamanho

--- src/ContainerPai.jsx
+++ src/ContainerPai.jsx
@@ -1,44 +1,41 @@
 import './Perfil.css'
 import Title from "./Title"
-import Button from './Button';
+import Button from './Button'; 
 function ContainerPai({nome}){
     return(
         <div className='card'>
            <Title />
             <p className='cardTitle'>{nome}</p>
     <div className="caixa" >
-                <p className='texto'>10/10/2020 08:13</p>
-                <p>Notícias - Zona Norte ... Notícias - São Miguel do Guaporé ...
-                O Instituto Federal de Educação, Ciência e Tecnologia de Rondônia
-                IFRO vem a público"</p>
+                <p className='texto'>01/06/2022 08:13</p>
+                <p>NOTA A COMUNIDADE:
+                      Bloqueio do orçamento da
+                      Educação</p>
                 
             </div>
             <div className="caixa" >
-                <p className='texto'>10/10/2020  08:13</p>
-                <p>Notícias - Zona Norte ... Notícias - São Miguel do Guaporé ...
-                O Instituto Federal de Educação, Ciência e Tecnologia de Rondônia
-                IFRO vem a público"</p>
+                <p className='texto'>31/05/2022 12:35</p>
+                <p>IFRO
+                      publica edital para cadastro de 
+                      Interesse de Remoção</p>
                 
             </div>
             <div className="caixa" >
-                <p className='texto'>10/10/2020  08:13</p>
-                <p>Notícias - Zona Norte ... Notícias - São Miguel do Guaporé ...
-                O Instituto Federal de Educação, Ciência e Tecnologia de Rondônia
-                IFRO vem a público"</p>
+                <p className='texto'>30/05/2022 09:54</p>
+                <p>Parceria entre IFRO e UAB irá 
+                    ampliar a oferta de cursos de pós-graduação e de Pedagogia</p>
                 
             </div>
             <div className="caixa" >
-                <p className='texto'>10/10/2020  08:13</p>
-                <p>Notícias - Zona Norte ... Notícias - São Miguel do Guaporé ...
-                O Instituto Federal de Educação, Ciência e Tecnologia de Rondônia
-                IFRO vem a público" </p>
+                <p className='texto'>25/05/2022 15:10</p>
+                <p>Manutenção do Sistema SUAP 
+                    acontece nesta sexta-feira (27)</p>
                 
             </div>
             <div className="caixa" >
-                <p className='texto'>10/10/2020  08:13</p>
-                <p>Notícias - Zona Norte ... Notícias - São Miguel do Guaporé ...
-                O Instituto Federal de Educação, Ciência e Tecnologia de Rondônia
-                IFRO vem a público"</p>
+                <p className='texto'> 25/05/2022 15:10</p>
+                <p>Pais e responsáveis por alunos podem ter acesso a 
+                    informações estudantis pelo app IFRO Mobile</p>
                 
             </div>
                 <Button />
